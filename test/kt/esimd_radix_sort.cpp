2972e84d54f311a9225f4ce0e01fb0d6730e8e62
Sergey Kopienko <sergey.kopienko@intel.com>
2023-08-11 14:29:18 +0200

    @@@fenerate source data from 0 to (size - 1)

--- test/kt/esimd_radix_sort.cpp
+++ test/kt/esimd_radix_sort.cpp
@@ -212,7 +212,8 @@ generate_data(T* input, std::size_t size)
         //std::generate(input, input + unique_threshold, [&]{ return dist(gen); });
         for (std::size_t i = 0; i < size; ++i)
         {
-            input[i] = size - i - 1;
+            //input[i] = size - i - 1;
+            input[i] = i;
         }
     }
     else
